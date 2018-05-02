bc3a968c2bfdde8eb08b10bb7d01ed2396ea50b1
ankurjuneja <ankurjuneja05@gmail.com>
2018-05-01 23:29:29 -0700

    user service put

--- src/main/java/services/UserService.java
+++ src/main/java/services/UserService.java
@@ -4,8 +4,10 @@ import models.User;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -32,6 +34,19 @@ public class UserService {
 			userRepository.save(user);
 		}
 		return "saved";
+	}
+	
+	@RequestMapping(value = "/users/{userId}", method = RequestMethod.PUT)
+	public String updateUser (@RequestBody User user, @PathVariable long userId) {
+		Optional<User> userOptional = userRepository.findById(userId);
+		
+		if(!userOptional.isPresent())
+			return "no such user";
+		
+		user.setUserId(userId);
+		userRepository.save(user);
+		
+		return "updated";
 	}
 
 }
